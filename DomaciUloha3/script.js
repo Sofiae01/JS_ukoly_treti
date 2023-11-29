6450f002f41e49379337ec8c34f19bbf75e44fde
Sofiae01 <105079647+Sofiae01@users.noreply.github.com>
2023-11-29 19:54:46 +0100

    Update script.js

--- DomaciUloha3/script.js
+++ DomaciUloha3/script.js
@@ -1,13 +1,9 @@
 function parseDate(dateString) {
     const dateParts = dateString.split('.')
     const [day, month, year] = dateParts
-    return {
-      day: parseInt(day),
-      month: parseInt(month),
-      year: parseInt(year)
-    }
+    return {day,month,year}
   }
   const inputDate = '12.03.2015'
   const parsedDate = parseDate(inputDate)
-  document.body.innerHTML =+ parsedDate
+  document.body.innerHTML += `<p>Day: ${parsedDate.day}, Month: ${parsedDate.month}, Year: ${parsedDate.year}</p>`
 
